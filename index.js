440e8d873cbfaef8ab4940f71af4ab83f0fca115
Abi Beggs <beggsaj@gmail.com>
2021-12-12 10:55:05 -0600

    added logic for intern and engineer questions

--- index.js
+++ index.js
@@ -27,68 +27,77 @@ const questions = [{
         message: 'Enter your office number:',
     },
     {
-        type: 'checkbox',
-        name: 'job',
-        message: 'Select new team member:',
-        choices: ['Engineer','Intern','Finish Building Team']
-    }
+        type: 'input',
+        name: 'location',
+        message: 'Enter your office location:',
+    },
+    {
+        type: 'confirm',
+        name: 'addteam',
+        message: 'Do you need to add any additional team members',
+    },
+    {
+        type: 'list',
+        name: 'employeetype',
+        message: 'Select new team member :',
+        when: jobTitle('addteam'),
+        choices: ['Engineer', 'Intern', 'Finish Building Team'],
+    },
+    {
+        type: 'input',
+        name: 'engineername',
+        message: 'Enter Engineer Name',
+        when: (answers) => answers.employeetype === 'Engineer'
+    },
+    {
+        type: 'input',
+        name: 'engineerid',
+        message: 'Enter Engineer ID',
+        when: (answers) => answers.employeetype === 'Engineer'
+    },
+    {
+        type: 'input',
+        name: 'engineeremail',
+        message: 'Enter Engineer Email',
+        when: (answers) => answers.employeetype === 'Engineer'
+    },
+    {
+        type: 'input',
+        name: 'engineergithub',
+        message: 'Enter Engineer Github',
+        when: (answers) => answers.employeetype === 'Engineer'
+    },
+    {
+        type: 'input',
+        name: 'internname',
+        message: 'Enter Intern Name',
+        when: (answers) => answers.employeetype === 'Intern'
+    },
+    {
+        type: 'input',
+        name: 'internid',
+        message: 'Enter Intern id',
+        when: (answers) => answers.employeetype === 'Intern'
+    },
+    {
+        type: 'input',
+        name: 'internemail',
+        message: 'Enter Intern Email',
+        when: (answers) => answers.employeetype === 'Intern'
+    },
+    {
+        type: 'input',
+        name: 'internschool',
+        message: 'Enter Intern School',
+        when: (answers) => answers.employeetype === 'Intern'
+    },
 ];
 
-const internQuestions = [{
-    type: 'input',
-    name: 'name',
-    message: 'Enter your name:',
-},
-{
-    type: 'input',
-    name: 'github',
-    message: 'Enter your employee ID:',
-},
-{
-    type: 'input',
-    name: 'email',
-    message: 'Enter your email address:',
-},
-{
-    type: 'input',
-    name: 'description',
-    message: 'Enter your office number:',
-},
-{
-    type: 'checkbox',
-    name: 'job',
-    message: 'Select new team member:',
-    choices: ['Engineer','Intern','Finish Building Team']
+function jobTitle(add) {
+    return function (answers) {
+        return answers[add];
+    };
 }
-];
-
-const engineerQuestions = [{
-    type: 'input',
-    name: 'name',
-    message: 'Enter your name:',
-},
-{
-    type: 'input',
-    name: 'github',
-    message: 'Enter your employee ID:',
-},
-{
-    type: 'input',
-    name: 'email',
-    message: 'Enter your email address:',
-},
-{
-    type: 'input',
-    name: 'description',
-    message: 'Enter your office number:',
-},
-{
-    type: 'checkbox',
-    name: 'job',
-    message: 'Select new team member:',
-    choices: ['Engineer','Intern','Finish Building Team']
-}
-];
 
 // function to write HTML file
 function writeToFile(fileName, data) {

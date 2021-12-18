b275a6890da1833c1fb43bcbca4cecd3e16db5e0
Abi Beggs <beggsaj@gmail.com>
2021-12-18 08:42:29 -0600

    broke down manager and employee questions into separate functions

--- index.js
+++ index.js
@@ -1,99 +1,111 @@
 //packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer')
+
 const Manager = require('./lib/Manager')
 const Intern = require('./lib/Intern')
 const Engineer = require('./lib/Engineer')
 
-//array of questions for user input
-const questions = [{
-        type: 'input',
-        name: 'name',
-        message: 'Enter your name:',
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'Enter your employee ID:',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Enter your email address:',
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Enter your office number:',
-    },
-    {
-        type: 'confirm',
-        name: 'addteam',
-        message: 'Do you need to add any additional team members',
-    },
-    {
-        type: 'list',
-        name: 'employeetype',
-        message: 'Select new team member :',
-        when: jobTitle('addteam'),
-        choices: ['Engineer', 'Intern', 'Finish Building Team'],
-    },
-    {
-        type: 'input',
-        name: 'engineername',
-        message: 'Enter Engineer Name',
-        when: (answers) => answers.employeetype === 'Engineer'
-    },
-    {
-        type: 'input',
-        name: 'engineerid',
-        message: 'Enter Engineer ID',
-        when: (answers) => answers.employeetype === 'Engineer'
-    },
-    {
-        type: 'input',
-        name: 'engineeremail',
-        message: 'Enter Engineer Email',
-        when: (answers) => answers.employeetype === 'Engineer'
-    },
-    {
-        type: 'input',
-        name: 'engineergithub',
-        message: 'Enter Engineer Github',
-        when: (answers) => answers.employeetype === 'Engineer'
-    },
-    {
-        type: 'input',
-        name: 'internname',
-        message: 'Enter Intern Name',
-        when: (answers) => answers.employeetype === 'Intern'
-    },
-    {
-        type: 'input',
-        name: 'internid',
-        message: 'Enter Intern id',
-        when: (answers) => answers.employeetype === 'Intern'
-    },
-    {
-        type: 'input',
-        name: 'internemail',
-        message: 'Enter Intern Email',
-        when: (answers) => answers.employeetype === 'Intern'
-    },
-    {
-        type: 'input',
-        name: 'internschool',
-        message: 'Enter Intern School',
-        when: (answers) => answers.employeetype === 'Intern'
-    },
-    {
-        type: 'confirm',
-        name: 'addteamagain',
-        message: 'Do you need to add any additional team members',
-    },
-    
-];
+const completeTeam = []
+let manager;
+let teamTitle
 
+//array of questions for user input
+
+function managerQuestions(){
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter your name:',
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your employee ID:',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email address:',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Enter your office number:',
+        }        
+    ])
+}
+
+function employeeQuestions(){
+    inquirer.prompt([
+        {
+            type: 'confirm',
+            name: 'addteam',
+            message: 'Do you need to add any additional team members',
+        },
+        {
+            type: 'list',
+            name: 'employeetype',
+            message: 'Select new team member :',
+            when: jobTitle('addteam'),
+            choices: ['Engineer', 'Intern', 'Finish Building Team'],
+        },
+        {
+            type: 'input',
+            name: 'engineername',
+            message: 'Enter Engineer Name',
+            when: (answers) => answers.employeetype === 'Engineer'
+        },
+        {
+            type: 'input',
+            name: 'engineerid',
+            message: 'Enter Engineer ID',
+            when: (answers) => answers.employeetype === 'Engineer'
+        },
+        {
+            type: 'input',
+            name: 'engineeremail',
+            message: 'Enter Engineer Email',
+            when: (answers) => answers.employeetype === 'Engineer'
+        },
+        {
+            type: 'input',
+            name: 'engineergithub',
+            message: 'Enter Engineer Github',
+            when: (answers) => answers.employeetype === 'Engineer'
+        },
+        {
+            type: 'input',
+            name: 'internname',
+            message: 'Enter Intern Name',
+            when: (answers) => answers.employeetype === 'Intern'
+        },
+        {
+            type: 'input',
+            name: 'internid',
+            message: 'Enter Intern id',
+            when: (answers) => answers.employeetype === 'Intern'
+        },
+        {
+            type: 'input',
+            name: 'internemail',
+            message: 'Enter Intern Email',
+            when: (answers) => answers.employeetype === 'Intern'
+        },
+        {
+            type: 'input',
+            name: 'internschool',
+            message: 'Enter Intern School',
+            when: (answers) => answers.employeetype === 'Intern'
+        },
+        {
+            type: 'confirm',
+            name: 'addteamagain',
+            message: 'Do you need to add any additional team members',
+        }
+    ])
+}
 
 function jobTitle(add) {
     return function (answers) {
@@ -102,13 +114,19 @@ function jobTitle(add) {
 }
 
 // function to write HTML file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) => {
-        if (err)
-            throw err;
-        console.log('saved')
+const writeFile = data => {
+    fs.writeFile('./index.html', data, err => {
+        if (err) {
+            console.log(err)
+            return
+        } else {
+            console.log('success!')
+        }
     })
 }
+
+questions()
+
 
 function generateHTML(data) {
     return `
